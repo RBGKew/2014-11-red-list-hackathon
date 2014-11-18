ed92c3d0ac7a02344210e61897bf8013b510ad7b
Matthew Blissett <M.Blissett@kew.org>
2014-11-18 17:37:46 +0000

    Added data and visualisation.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-redlist-plants
-==============
+Endangered Plant Species Visualisation
+======================================
+
+This is a visualisation created at the [IUCN Red List Conservation Hackathon](http://conservationhackathon.org/hack-the-iucn-redlist/) held in November 2014.
+
+It reads in plant data from the [Sampled Red List Index](http://threatenedplants.myspecies.info/) and outputs a summary in JSON format.
+
+See <http://rbgkew.github.io/2014-11-red-list-hackathon/> for the resulting visualisation.
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import json
 import csv
 
+# Work from the Hack the IUCN Red List Conservation Hackathon: <http://conservationhackathon.org/hack-the-iucn-redlist/>
+#
+# See <http://rbgkew.github.io/2014-11-red-list-hackathon/>.
+#
+# Reads in data from the Sampled Red List Index of plants: <http://threatenedplants.myspecies.info/>.
+# Outputs summarised data in JSON format for use in visualisation, keyed by ISO 3166-1 Numeric country code.
+
+global isoCountryCodes
+
 # def readCsv(file,header=False):
 # 	with open(file,'r') as f:
 # 		lines = f.readlines()
@@ -8,10 +17,10 @@ import csv
 # 		lines.pop(0)
 # 	return [line.strip().split(',') for line in lines]
 
-def readCsv(file,header=False):
+def readCsv(file,header=False,delimiter=','):
 	result = []
 	with open(file,'r') as f:
-		reader = csv.reader(f)
+		reader = csv.reader(f, delimiter=delimiter)
 		for row in reader:
 			result.append(row)
 	return result
@@ -33,10 +42,17 @@ def uniq(seq):
     seen_add = seen.add
     return [ x for x in seq if not (x in seen or seen_add(x))]
 
+# Convert list of possible country codes (alpha2) into JSON of numeric codes with name and alpha2 code,
+# including only valid countries.
 def createJson(countries):
 	jData = {}
 	for country in countries:
-		jData[country] = {}
+		isoData = isoCountryData(country)
+		if (isoData):
+			jData[isoData['numeric']] = {
+				'name' : isoData['name'],
+				'alpha2' : isoData['alpha2']
+			}
 	return jData
 
 def extractSpeciesForCountry(country,cData):
@@ -69,27 +85,42 @@ def extractThreatTypes(speciesName,tData):
 		threatTypes.append(row[8])
 	return threatTypes
 
+# Given an ISO Alpha-2 country code (e.g. GB) return the name, alpha2 code and numeric code (e.g. 826).
+# (Inefficient, replace with pycountry.)
+def isoCountryData(country):
+	for row in isoCountryCodes:
+		if (country == row[1]):
+			isoData = {
+				'name': row[0],
+				'alpha2': row[1],
+				'numeric': row[2]
+			}
+			return isoData
+
 def writeJson(jData,jsonFile):
-	json.dump(jData,open(jsonFile,'w'))
+	json.dump(jData,open(jsonFile,'w'),indent=4,separators=(',', ': '))
 
 
 if __name__ == "__main__":
 
-	countryCSV = 'CSV/02.txt'
-	threatCSV = 'CSV/01.txt'
+	countryCSV = 'data/02_coo_published.csv'
+	threatCSV = 'data/01_threats_species_published.csv'
+	isoCountriesCSV = 'data/iso-countries.tsv'
 
 	cData = readCsv(countryCSV)
 	tData = readCsv(threatCSV)
 
+	isoCountryCodes = readCsv(isoCountriesCSV, delimiter='\t')
+
 	jData = createJsonCountries(cData)
 
-	for country in jData:
-		sData = extractSpeciesForCountry(country,cData)	
+	for countryCode, countryData in jData.iteritems():
+		origCountry = countryData['alpha2']
+		sData = extractSpeciesForCountry(origCountry,cData)
 		species = {}
 		speciesNames = [row[2] for row in sData]
 		for name in speciesNames:
 			species[name]=sorted(uniq(extractThreatTypes(name,tData)))
-		jData[country] = species
+		jData[countryCode] = { 'country': countryData, 'species': species }
 
-	writeJson(jData,'countries.json')
-
+	writeJson(jData,'output/srli-data.json')
